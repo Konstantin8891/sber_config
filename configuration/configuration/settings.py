d45127a373eeb9f8b153d161de2705f13af2eb0b
Konstantin8891 <89670253660@mail.ru>
2022-11-03 13:38:19 +0300

    Add postgresql

--- configuration/configuration/settings.py
+++ configuration/configuration/settings.py
@@ -68,9 +68,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'configuration.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_NAME", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", 5432),
     }
 }
 
